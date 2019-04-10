5aabdd70b160fc0bf3832447cab61a1e434ba1f7
Danilo Gomes <dan.theslashd@gmail.com>
2019-04-09 22:58:21 -0300

    [INC] db.pp - Creates loja-schema, loja-user and remove anonymous user

--- manifests/db.pp
+++ manifests/db.pp
@@ -25,3 +25,26 @@ service { "mysql":
   require    => Package["mysql-server"],
 }
 
+exec { "loja-schema":
+  #Comando de teste, caso a saida seja do codigo seja 0, o comando nao executa.
+  #Ao contrario do exec apt-get update, neste caso eh importante que o Puppet nao
+  # tente criar um novo schema toda vez que for executado
+  unless  => "mysql -u root loja_schema",
+  command => "mysqladmin -u root create loja_schema",
+  path    => "/usr/bin",
+  require => Service["mysql"],
+}
+
+exec { "remove-anonymous-user":
+  command => "mysql -u root -e \"DELETE FROM mysql.user WHERE user=''; FLUSH PRIVILEGES\"",
+  onlyif  => "mysql -u ''", #Executa apenas se a saida do teste for 0
+  path    => "/usr/bin",
+  require => Service["mysql"],
+}
+
+exec { "loja-user":
+  unless  => "mysql -u loja -p lojasecret loja_schema",
+  command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON loja_schema.* TO 'loja'@'%' IDENTIFIED BY 'lojasecret';\"",
+  path    => "/usr/bin",
+  require => Exec["loja-schema"],
+}
